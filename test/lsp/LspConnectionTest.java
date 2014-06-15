efea4c7087fe5e0de5820210163d78a2308c0cbf
Wagner Macedo <wagnerluis1982@gmail.com>
2014-06-14 21:17:43 -0300

    Verificando ação de epochTriggers

--- test/lsp/LspConnectionTest.java
+++ test/lsp/LspConnectionTest.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 
 public class LspConnectionTest {
 	int time;
+	int epoch;
 	boolean closed;
 
 	/*
@@ -16,16 +17,18 @@ public class LspConnectionTest {
 	@Test
 	public void testStatusChecker() throws InterruptedException {
 		// LspConnection com epoch = 2 msec e epochLimit = 3, portanto a thread
-		// durará 8 msec e deve incrementar o valor de time a cada 2 msec e
+		// durará 8 msec e deve incrementar o valor de time e decrementar o
+		// valor de epoch a cada 2 msec e
 		// alterar closed para true no final.
 		time = 0;
+		epoch = 4;
 		closed = false;
 		new LspConnection((short) 1, new LspParams(2, 3), new Actions() {
 			public long lastReceiptTime() {
 				return 1 / ++time;
 			}
 			public void epochTriggers() {
-				// Irrelevante para esse teste
+				epoch--;
 			}
 			public void closeConnection() {
 				closed = true;
@@ -34,11 +37,13 @@ public class LspConnectionTest {
 
 		// Ainda não houve incremento
 		assertEquals(0, time);
+		assertEquals(4, epoch);
 		assertFalse(closed);
 
-		// Ao finalizar
+		// Ao finalizar...
 		Thread.sleep(11);
-		assertEquals(5, time);	// garante que não houve mais incrementos
+		assertEquals(5, time);	// garante não haver mais incrementos
+		assertEquals(0, epoch);	// garante não haver mais decrementos
 		assertTrue(closed);		// garante que a "conexão" foi fechada
 	}
 }
