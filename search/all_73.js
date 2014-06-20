0f290f772055bf9a9e70a805a7887c57f8e6725c
Wagner Macedo <wagnerluis1982@gmail.com>
2014-06-20 19:29:24 -0300

    LspSocket simplificado

--- search/all_73.js
+++ search/all_73.js
@@ -1,6 +1,5 @@
 var searchData=
 [
-  ['send',['send',['../classlsp_1_1LspSocket.html#a8ef8eba349321933dd9f547319048130',1,'lsp::LspSocket']]],
   ['sendack',['sendAck',['../classlsp_1_1LspSocket.html#ad3efe69ecc109b842f809bd1f5133c21',1,'lsp.LspSocket.sendAck(final LspConnection conn, final short seqNum)'],['../classlsp_1_1LspSocket.html#ad5c76c6da8f25d2c1fb3ddceeb809835',1,'lsp.LspSocket.sendAck(final InternalPack p)']]],
   ['sendconnect',['sendConnect',['../classlsp_1_1LspSocket.html#a930888d52151cbe08bedd852100441b0',1,'lsp::LspSocket']]],
   ['senddata',['sendData',['../classlsp_1_1LspSocket.html#a49f21a421ffbcce1895b219c69b3000d',1,'lsp.LspSocket.sendData(final LspConnection conn, final short seqNum, final byte[] payload)'],['../classlsp_1_1LspSocket.html#a529bf9db3b181f5f6b74dbf946d756d2',1,'lsp.LspSocket.sendData(final InternalPack p)']]],
@@ -9,6 +8,5 @@ var searchData=
   ['serverecho_2ejava',['ServerEcho.java',['../ServerEcho_8java.html',1,'']]],
   ['setconnection',['setConnection',['../classlsp_1_1InternalPack.html#a610109fe137dc417a4dd65f94c1bec25',1,'lsp::InternalPack']]],
   ['setconnid',['setConnId',['../classlsp_1_1Pack.html#a24373aa7bd19bfbe408b5125169c7230',1,'lsp::Pack']]],
-  ['setseqnum',['setSeqNum',['../classlsp_1_1InternalPack.html#a05ab0be22fe5bf44008a9060ef124a4f',1,'lsp::InternalPack']]],
-  ['sockaddr',['sockAddr',['../classlsp_1_1LspSocket.html#acb83d6c2146566a2c60315e01f0f0c07',1,'lsp::LspSocket']]]
+  ['setseqnum',['setSeqNum',['../classlsp_1_1InternalPack.html#a05ab0be22fe5bf44008a9060ef124a4f',1,'lsp::InternalPack']]]
 ];
--- search/functions_75.js
+++ search/functions_75.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['uniquesockid',['uniqueSockId',['../classlsp_1_1LspConnection.html#ac7f30bb81ea1f85a4981f21fbab35cc9',1,'lsp::LspConnection']]]
+  ['uniquesockid',['uniqueSockId',['../classlsp_1_1LspConnection.html#ac7f30bb81ea1f85a4981f21fbab35cc9',1,'lsp::LspConnection']]],
+  ['usedconnection',['usedConnection',['../classlsp_1_1LspSocket.html#a080c414431aadd88d42a2588b465fa26',1,'lsp::LspSocket']]]
 ];
