16eafef852db2a1f1e9e39db3216332599e8d33b
Wagner Macedo <wagnerluis1982@gmail.com>
2014-06-25 09:40:54 -0300

    Limpeza de código

--- search/all_69.js
+++ search/all_69.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['incsendmissing',['incSendMissing',['../classlsp_1_1LspConnection.html#a3e55ece7779a78f6bda1ae2287cc181e',1,'lsp::LspConnection']]],
-  ['internalpack',['InternalPack',['../classlsp_1_1InternalPack.html#a7d72ba648f8457d38e2af0817392cc5b',1,'lsp.InternalPack.InternalPack(LspConnection conn, short seqNum, byte[] payload)'],['../classlsp_1_1InternalPack.html#a8782c766573a9d2a8b04c2bc3342b303',1,'lsp.InternalPack.InternalPack(LspConnection conn, byte[] payload)'],['../classlsp_1_1InternalPack.html#a23ba4e465f7ad35464f3fb1c686b967c',1,'lsp.InternalPack.InternalPack(Pack pack)'],['../classlsp_1_1InternalPack.html#a4c97d2a84f4f24bd909208bb1b9ae2df',1,'lsp.InternalPack.InternalPack(short connId, byte[] payload)']]],
+  ['internalpack',['InternalPack',['../classlsp_1_1InternalPack.html#a7d72ba648f8457d38e2af0817392cc5b',1,'lsp::InternalPack']]],
   ['internalpack',['InternalPack',['../classlsp_1_1InternalPack.html',1,'lsp']]],
   ['internalpack_2ejava',['InternalPack.java',['../InternalPack_8java.html',1,'']]],
   ['isactive',['isActive',['../classlsp_1_1LspSocket.html#a4acae955bc00d318bef18d6387743bf4',1,'lsp::LspSocket']]],
--- search/all_73.js
+++ search/all_73.js
@@ -2,7 +2,5 @@ var searchData=
 [
   ['send',['send',['../classlsp_1_1LspSocket.html#a892328b3713975018f942b9a9ec337f3',1,'lsp::LspSocket']]],
   ['sent',['sent',['../classlsp_1_1LspConnection.html#af8473bc5a9579672d11aea7a36f290ac',1,'lsp.LspConnection.sent()'],['../classlsp_1_1LspConnection.html#a33e3b98829b6adfb024ee4339c9e6bce',1,'lsp.LspConnection.sent(Pack pack)']]],
-  ['setconnection',['setConnection',['../classlsp_1_1InternalPack.html#a610109fe137dc417a4dd65f94c1bec25',1,'lsp::InternalPack']]],
-  ['setconnid',['setConnId',['../classlsp_1_1Pack.html#a24373aa7bd19bfbe408b5125169c7230',1,'lsp::Pack']]],
-  ['setseqnum',['setSeqNum',['../classlsp_1_1InternalPack.html#a05ab0be22fe5bf44008a9060ef124a4f',1,'lsp::InternalPack']]]
+  ['setconnid',['setConnId',['../classlsp_1_1Pack.html#a24373aa7bd19bfbe408b5125169c7230',1,'lsp::Pack']]]
 ];
